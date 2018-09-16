aa0e0ad6b89e6e322f7c2e7046d1223fc168f4b8
Frank Odoom <f.odoom@outlook.com>
2018-09-16 13:28:49 +0000

    Initial Commit

---
+++ MainActivity.cs
@@ -0,0 +1,60 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using Android.Content.PM;
+using Java.Util;
+using Android.Util;
+using System;
+
+namespace Android.Localization
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.Locale)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnTranslate;
+        TextView txtMessage;
+        Spinner spinner;
+        Android.Content.Res.Resources res;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btnTranslate = FindViewById<Button>(Resource.Id.btnTranslate);
+            txtMessage = FindViewById<TextView>(Resource.Id.textMessage);
+            spinner = FindViewById<Spinner>(Resource.Id.select_language);
+
+            
+
+
+            btnTranslate.Click += BtnTranslate_Click;
+
+         
+            Context context = this; // Get the Resources object from our context
+            res = context.Resources; // Get the string 
+            Content.Res.Configuration conf = res.Configuration;
+            DisplayMetrics dm = res.DisplayMetrics;
+            conf.SetLocale(new Locale("ak")); //Set local to Akan ak
+        
+            res.UpdateConfiguration(conf, dm);
+
+            var lang = Resources.Configuration.Locale;
+            var country = lang.GetDisplayCountry(lang);
+            Toast.MakeText(this, "Your current locale is " + lang, ToastLength.Long).Show();
+            Toast.MakeText(this, "Country " + country, ToastLength.Long).Show();
+            Toast.MakeText(this, "Display Name " + lang.DisplayName, ToastLength.Long).Show();
+            Toast.MakeText(this, "Language " + lang.Language, ToastLength.Long).Show();
+        }
+
+        //
+        private void BtnTranslate_Click(object sender, System.EventArgs e)
+        {
+            string greet = res.GetString(Resource.String.greet_message);
+            txtMessage.Text = greet;          
+        }
+    }
+}
---
+++ obj/Debug/MonoAndroid81/android/src/md57220147f3e82e6884017d618f221023b/MainActivity.java
@@ -0,0 +1,47 @@
+package md57220147f3e82e6884017d618f221023b;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Android.Localization.MainActivity, Android.Localization", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Android.Localization.MainActivity, Android.Localization", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
